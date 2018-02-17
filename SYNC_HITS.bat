625c85ea632b4329bb24f02790ed40f68a7043b0
Daehyun You <daehyun@dc.tohoku.ac.jp>
2018-02-17 18:23:49 +0900

    Update the batch file to update data

--- SYNC_HITS.bat
+++ SYNC_HITS.bat
@@ -1,4 +1,4 @@
 :begin
-"C:\Program Files (x86)\WinSCP\WinSCP.exe" /init=nul /log="C:\Users\uedalab\Desktop\RESORT_SCRIPT\SYNC_HITS.log" /script="C:\Users\uedalab\Desktop\RESORT_SCRIPT\SYNC_HITS_CONFIG.txt"
+"C:\Program Files (x86)\WinSCP\WinSCP.com" /init=nul /log="C:\Users\uedalab\Desktop\RESORT_SCRIPT\SYNC_HITS.log" /script="C:\Users\uedalab\Desktop\RESORT_SCRIPT\SYNC_HITS_CONFIG.txt"
 TIMEOUT 10
 goto begin
