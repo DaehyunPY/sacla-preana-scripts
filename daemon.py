cd752614324acc029101539d4c475de76bf38a54
Daehyun You <daehyun@dc.tohoku.ac.jp>
2018-07-16 21:45:01 +0900

    Update daemon.py, preanalyze.py and template.txt

--- daemon.py
+++ daemon.py
@@ -1,55 +1,78 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # %% import external dependencies
 from glob import glob
 from os import remove
-from os.path import dirname, join, basename, splitext, exists
-from typing import List, Set
+from os.path import dirname, join, basename, splitext, exists, relpath, getmtime
+from typing import List, Set, Mapping
 from time import sleep
-from datetime import datetime
+from datetime import datetime, timedelta
 from threading import Thread, active_count
+from itertools import groupby
+
+from preanalyze import call_preanalyzer
 
 
 # %% parameters
-maxworkers = 3
+maxworkers = 4
+startinterval = 30
 
 
-def targetlist() -> Set[str]:
+def workingdir(key: str) -> str:
+    return f'C:\\Users\\uedalab\\Desktop\\preanalysis_daehyun\\test\\hit_files\\{key}'
+
+
+def keypatt(lmafilename: str) -> str:
+    key, _ = basename(lmafilename).rsplit('__', maxsplit=1)
+    return key
+
+
+def currentkeys() -> Mapping[str, float]:
     patt = 'C:\\Users\\uedalab\\Desktop\\preanalysis_daehyun\\test\\lma_files\\*.lma'
-    return set(glob(patt))
+    globbed = glob(patt)
+    return  {k: max(getmtime(f) for f in groupped)
+             for k, groupped in groupby(globbed, keypatt) if not exists(workingdir(k))}
 
 
-def outputpath(target: str) -> str:
-    par = dirname(target)
-    key, _ = splitext(basename(target))
-    return join(par, '..', 'hit_files', key)
+def targetlist(key: str) -> List[str]:
+    patt = f'C:\\Users\\uedalab\\Desktop\\preanalysis_daehyun\\test\\lma_files\\{key}__*.lma'
+    globbed = glob(patt)
+    return sorted(globbed)
 
 
 # %%
 def todolist() -> Set[str]:
-    targets = targetlist()
-    return {t for t in targets if not exists(outputpath(t))}
+    print(f"[{datetime.now()}] Scanning lma files...")
+    lastkeys = currentkeys()
+    lastchecked = datetime.now()
+    while True:
+        if datetime.now() < lastchecked + timedelta(seconds=startinterval):
+            sleep(startinterval)
+            continue
+        print(f"[{datetime.now()}] Scanning new lma files...")
+        curr = currentkeys()
+        lastchecked = datetime.now()
+        yield sorted(k for k in curr if k in lastkeys and curr[k] <= lastkeys[k])
+        lastkeys = curr
 
 
-def islocked(target: str) -> bool:
-    out = outputpath(target)
-    locker = f'{out}.locked'
+def islocked(key: str) -> bool:
+    wdir = workingdir(key)
+    locker = f'{wdir}.locked'
     return exists(locker)
 
 
-def work(target: str) -> None:
-    out = outputpath(target)
-    locker = f'{out}.locked'
-    print(f"[{datetime.now()}] Working on target '{target}'...")
+def work(key: str) -> None:
+    wdir = workingdir(key)
+    locker = f'{wdir}.locked'
+    print(f"[{datetime.now()}] Working on key '{key}'...")
     with open(locker, 'w'):
-        with open(out, 'w'):
-            sleep(10)
+        call_preanalyzer(lmafilelist=targetlist(key), workingdir=wdir)
     remove(locker)
 
 
 # %% inf loop
-while True:
-    for target in todolist():
-        if not islocked(target) and active_count()-1 < maxworkers:
-            job = Thread(target=work, args=[target])
+for jobs in todolist():
+    for key in jobs:
+        if not islocked(key) and active_count()-1 < maxworkers:
+            job = Thread(target=work, args=[key])
             job.start()
-    sleep(30)
--- preanalyze.py
+++ preanalyze.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python3
 # %% import external dependencies
+from typing import List, Iterable
 from os import makedirs
-from os.path import join, basename, realpath
+from os.path import join, basename, realpath, relpath
 from textwrap import dedent
 from subprocess import call
+from warnings import warn
 
 
 __all__ = ['call_preanalyzer']
@@ -19,23 +20,26 @@ with open(templatepath, 'r') as f:
     template = f.read()
 
 
-def call_preanalyzer(lmafilename: str, workingdir: str) -> None:
-    lmafilename = realpath(lmafilename)
+def call_preanalyzer(lmafilelist: Iterable[str], workingdir: str) -> None:
     workingdir = realpath(workingdir)
+    lmafilelist = [relpath(f, workingdir) for f in lmafilelist]
+    goodlist = [f for f in lmafilelist if ' ' not in f]
+    if len(goodlist)!=len(lmafilelist):
+        warn('One or more lma files skipped because their paths include space!')
     makedirs(workingdir, exist_ok=True)
     key = basename(workingdir)
 
     with open(join(f'{workingdir}', 'resort.txt'), 'w') as f:
         f.write(template.format(
             basename=f'{key}',
-            lma_filelist=lmafilename,
+            lmafilelist='\n'.join(goodlist),
         ))
 
     with open(join(f'{workingdir}', 'resort.bat'), 'w') as f:
         f.write(dedent(
             f"""\
             pushd {workingdir}
-            {lma2rootpath} < resort.txt > resort.log
+            {lma2rootpath} < resort.txt 1> resort.log 2> resort.err
             """
         ))
     call(join(f'{workingdir}', 'resort.bat'))
