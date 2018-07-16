f85c299dfbdd94f0667c27b68d4df06fce9923eb
Daehyun You <daehyun@dc.tohoku.ac.jp>
2018-07-16 22:46:15 +0900

    Add route.bat

---
+++ route.bat
@@ -0,0 +1,13 @@
+:: run with sudo
+set eduroam=192.168.0.1
+:: change gateway ip!
+:: set daquser=172.30.121.254
+set daquser=172.30.39.254
+
+route delete 0.0.0.0
+route add 0.0.0.0 mask 0.0.0.0 %eduroam% metric 20
+route add %daquser% mask 255.255.254.0 %daquser% metrix 10
+route add 10.12.100.0 mask 255.255.254.0 %daquser% metric 10
+route add 172.30.120.0 mask 255.255.254.0 %daquser% metric 10
+route add 172.28.8.0 mask 255.255.254.0 %daquser% metric 10
+pause
