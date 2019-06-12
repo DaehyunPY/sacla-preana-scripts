38bad66027003d4971a93ed685eb9df9bfd2d318
Daehyun You <daehyun@dc.tohoku.ac.jp>
2019-06-12 15:14:38 +0900

    Backup for beamtime 2019A8045 Fukuzawa

--- route.bat
+++ route.bat
@@ -1,14 +1,10 @@
 REM run with sudo
-set eduroam=192.168.0.1
-REM change gateway ip!
 REM set daquser=172.30.121.254
-set daquser=172.30.39.254
-REM set daquser=172.30.103.254
+set daquser=172.30.103.254
+REM set daquser=172.30.39.254  REM SACLA BL3
 
-route delete 0.0.0.0
-route add 0.0.0.0 mask 0.0.0.0 %eduroam% metric 20
-route add %daquser% mask 255.255.254.0 %daquser% metrix 10
-route add 10.12.100.0 mask 255.255.254.0 %daquser% metric 10
-route add 172.30.120.0 mask 255.255.254.0 %daquser% metric 10
-route add 172.28.8.0 mask 255.255.254.0 %daquser% metric 10
+route add %daquser% mask 255.255.254.0 %daquser% metrix 100
+route add 10.12.100.0 mask 255.255.254.0 %daquser% metric 100
+route add 172.30.120.0 mask 255.255.254.0 %daquser% metric 100
+route add 172.28.8.0 mask 255.255.254.0 %daquser% metric 100
 pause
--- run.py
+++ run.py
@@ -3,11 +3,11 @@ from daemon import call_aprocess
 
 
 lmafilelist = """
-Z:\\2018B8014Kukk\\lma_files\\aq000__0000.lma
-Z:\\2018B8014Kukk\\lma_files\\aq000__0001.lma
-Z:\\2018B8014Kukk\\lma_files\\aq000__0002.lma
+Z:\\2019A8045Fukuzawa\\lma_files\\aq000__0000.lma
+Z:\\2019A8045Fukuzawa\\lma_files\\aq000__0001.lma
+Z:\\2019A8045Fukuzawa\\lma_files\\aq000__0002.lma
 """.split()
-workingdir = "D:\\2018B8014Kukk\\hit_files\\aq000_0000--0002"
+workingdir = "D:\\2019A8045Fukuzawa\\hit_files\\aq000_0000--0002"
 
 call_aprocess(
     lmafilelist=lmafilelist,
