4e00ff9201d04055b8b52d8449b8cfaf8e8838f9
Daehyun You <daehyun@dc.tohoku.ac.jp>
2019-10-17 10:47:59 +0900

    Update for SACLA beamtime 2019B8035 Fukuzawa

--- route.bat
+++ route.bat
@@ -1,5 +1,5 @@
 REM Run with sudo
-set daquser=172.30.39.254
+set daquser=172.30.103.254
 
 route add %daquser% mask 255.255.254.0 %daquser% metrix 100
 route add 10.12.100.0 mask 255.255.254.0 %daquser% metric 100
--- sync.bat
+++ sync.bat
@@ -1,4 +1,5 @@
 :begin
 "C:\Program Files (x86)\WinSCP\WinSCP.com" /init=nul /script="sync.txt"
+rclone copy -P L:\home\data\2019B8035Fukuzawa\ google-2019b8035fukuzawa:"/Experimental data/Online/"
 TIMEOUT 60
 goto begin
