ff4d722f624649f2c999e0c8ee890ceff4c75c84
Daehyun You <daehyun@dc.tohoku.ac.jp>
2019-10-18 20:36:28 +0900

    Fix a bug

--- sync.bat
+++ sync.bat
@@ -1,5 +1,5 @@
 :begin
 "C:\Program Files (x86)\WinSCP\WinSCP.com" /init=nul /script="sync.txt"
-rclone copy -P L:\home\data\2019B8035Fukuzawa\ "G:\Shared drives\SACLA 2019B8035 Fukuzawa\Experimental data\Online\"
+rclone copy -P L:\home\data\2019B8035Fukuzawa\ "google-2019b8035fukuzawa:/Experimental data/Online/"
 TIMEOUT 60
 goto begin
